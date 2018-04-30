5efcd24359a1553d913199591f2ad1adfaea54f3
vkarppinen <karppinenvaltteri@gmail.com>
2018-04-30 16:41:38 +0300

    configure subscriber to handle redirection to quality check view

--- src/plone/qualidator/behaviors/qualidator.py
+++ src/plone/qualidator/behaviors/qualidator.py
@@ -1,22 +1,27 @@
 from plone.autoform.interfaces import IFormFieldProvider
-from zope.interface.declarations import alsoProvides
-from plone.supermodel import directives
+from zope.interface.declarations import provider
 from plone.supermodel import model
-from zope import schema
+import transaction
 
 
+@provider(IFormFieldProvider)
 class IQualidatable(model.Schema):
-    directives.fieldset(
-        'social',
-        label=u'Social',
-        fields=('lanyrd',),
+    pass
+
+
+def do_redirect(trans, obj=None):
+    request = getattr(obj, 'REQUEST', None)
+    if request:
+        url = obj.absolute_url() + '/@@content_qa'
+        request.response.redirect(url)
+
+
+def handle_redirect(object, event):
+    """
+    Subbscriber handler for qualidator behavior.
+    Redirects to content quality view.
+    """
+    kwargs = dict(
+        obj=object,
     )
-
-    lanyrd = schema.URI(
-        title=u"Lanyrd link",
-        description=u"Add URL",
-        required=False,
-    )
-
-
-alsoProvides(IQualidatable, IFormFieldProvider)
+    transaction.get().addAfterCommitHook(do_redirect, kws=kwargs)
